ffb60eb370afde06cfe8989e5cdee9d8f6f526dd
Tejaswini Belashe <sonu2201.beleshe@gmail.com>
2025-01-07 22:49:59 +0530

    worked on Thunk

--- react-redux-toolkit/src/component/Product.jsx
+++ react-redux-toolkit/src/component/Product.jsx
@@ -1,14 +1,17 @@
 import React,{useState,useEffect} from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
-import { useDispatch } from "react-redux";
+import { useDispatch ,useSelector} from "react-redux";
 import {add} from '../component/store/cartSlice'
+import { getProducts } from "./store/productSlice";
+import StatusCode from "./utils/StatusCode";
 const Product = ()=>{
-    const [products,getProducts] = useState([]);
+    // const [products,getProducts] = useState([]);
     const dispatch = useDispatch();
+    const allProducts = useSelector(state=>state.products)
     useEffect(()=>{
-        fetch('https://fakestoreapi.com/products').then(data=>data.json()).then(result=> getProducts(result))
-
+        // fetch('https://fakestoreapi.com/products').then(data=>data.json()).then(result=> getProducts(result))
+       dispatch(getProducts())
     },[])
 
     const addToCart = (product)=>{
@@ -16,7 +19,7 @@ const Product = ()=>{
       dispatch(add(product))
     }
     
-    const cards = products.map((product,i)=>(
+    const cards = allProducts.data.map((product,i)=>(
         <div className="col-sm-4 col-md-3 ">
         <Card style={{ width: '18rem'}} key={product.id} className="h-100">
       <Card.Img variant="top" src={product.image} style={{height:'12rem'}} />
@@ -32,6 +35,12 @@ const Product = ()=>{
       </Card>
       </div>
       ))
+      if (allProducts.status === StatusCode.LOADING){
+        return <p>Loading...</p>
+      }
+      if(allProducts.status === StatusCode.ERROR){
+        return <p>Error has occured</p>
+      }
     return(
         <div>
         <h1>product component</h1>
--- react-redux-toolkit/src/component/store/productSlice.js
+++ react-redux-toolkit/src/component/store/productSlice.js
@@ -1,22 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import StatusCode from "../utils/StatusCode";
 
 
 const productSlice = createSlice({
     name:'products',
     initialState:{
-        data:[]
+        data:[],
+        status:'idle'
     },
     reducers:{
-
+        // fetchProducts(state,action){
+        //     state.data = action.payload
+        // }
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(getProducts.pending,(state,action)=>{
+            state.status = 'loading'
+        })
+        .addCase(getProducts.rejected,(state,action)=>{
+            state.status = 'error'
+        })
+        .addCase(getProducts.fulfilled,(state,action)=>{
+            state.data = action.payload;
+            state.status = 'idle'
+        })
     }
 })
 
-export const {} = productSlice.actions
+export const {fetchProducts} = productSlice.actions
 export default productSlice.reducer;
 
-export function getProducts(){
-    return async function getProductsThunk(dispatch,getState){
-        
-    }
-}
+// export function getProducts(){
+//     return async function getProductsThunk(dispatch,getState){
+//         let data = await fetch('https://fakestoreapi.com/products');
+//         let result = await data.json();
+//         dispatch(fetchProducts(result))
+//     }
+// }
+
+export const getProducts = createAsyncThunk('product/get',async()=>{
+    const data = await fetch('https://fakestoreapi.com/products');
+    const result = await data.json();
+    return result;
+})
