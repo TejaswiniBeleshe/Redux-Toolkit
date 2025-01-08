373329b31a19367f1eb08047b6d725181c7ec6d1
Tejaswini Belashe <sonu2201.beleshe@gmail.com>
2025-01-09 00:55:44 +0530

    implemented restock action

--- Redux-demo/index.js
+++ Redux-demo/index.js
@@ -1,8 +1,10 @@
 // creating an action
 const redux = require('redux');
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 // action is an objection containing type property
 // {
@@ -18,6 +20,12 @@ function orderCake(){
     }
 }
 
+function restockCake(quant=1){
+    return{
+        type:CAKE_RESTOCKED,
+        payload:quant
+    }
+}
 // application state represented by single object
 
 const initialState = {
@@ -31,6 +39,11 @@ const reducer = (state=initialState,action)=>{
             return {
                 numberOfCake:state.numberOfCake-1
             }
+        case CAKE_RESTOCKED:
+            return{
+                ...state,
+                numberOfCake:state.numberOfCake+action.payload
+            }
         default:
             return state;
         
@@ -42,12 +55,19 @@ const store = createStore(reducer)
 
 // 2
 console.log('initial state',store.getState())
-
 // 4
 const unsubscribe = store.subscribe(()=>console.log('updated state',store.getState()))
 
 // 3
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+
+// store.dispatch(restockCake(3));
+
+const actions = bindActionCreators({orderCake,restockCake},store.dispatch);
+actions.orderCake()
+actions.orderCake()
+
+actions.restockCake(4)
 
 unsubscribe()
