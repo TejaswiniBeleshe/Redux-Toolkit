f99a8f0a534e37303f8da0b7e34c13c8a57d8e61
Tejaswini Belashe <sonu2201.beleshe@gmail.com>
2025-01-08 22:46:41 +0530

    action and action creator are created

--- Redux-demo/index.js
+++ Redux-demo/index.js
@@ -1,1 +1,17 @@
-console.log("from index.js")
+// creating an action
+
+const CAKE_ORDERED = 'CAKE_ORDERED';
+
+// action is an objection containing type property
+// {
+//     type:CAKE_ORDERED
+// }
+
+// action creator- is a function that returns object
+
+function orderCake(){
+    return{
+        type:CAKE_ORDERED,
+        quantity:1
+    }
+}
